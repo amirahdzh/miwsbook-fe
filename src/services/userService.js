127ad1fd4fcbb8bbeb1ed388953c784b37c61bde
amirahdzh <amirahdzh@gmail.com>
2025-03-19 19:06:39 +0700

    change end point user

--- src/services/userService.js
+++ src/services/userService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:8000/api/v1";
+const API_URL = "https://amiwspace.my.id/api/v1";
 
 // Function to get the auth token
 const getAuthToken = () => {
